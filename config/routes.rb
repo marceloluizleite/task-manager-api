4ffeba895cc5032a55ec0a3e63ef038d406a28b3
Marcelo Luiz Leite <marceloluiz.developer@gmail.com>
2017-06-07 18:49:21 -0400

    changing double quotes to simple quotes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: "/" do    
-    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do    
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show]
     end
-
-    # namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
-    #   resources :tasks
-    # end 
   end
 
 end
